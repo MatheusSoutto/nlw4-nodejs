0a114fb9b35a6cc07009ca3041c70b51b0ee1fd4
Matheus Soutto <matheussoutto27@gmail.com>
2021-02-28 19:09:42 -0300

    Day Two

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
-import express, { response } from 'express';
-
+import express from 'express';
+import 'reflect-metadata';
+import './database';
+import { router } from './routes';
+ 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World!' });
-});
+app.use(express.json());
 
-app.post('/', (request, response) => {
-  return response.json({ message: 'Dados salvos!' });
-});
+app.use(router);
 
 app.listen(3333, () => console.log('Server is running!'));
